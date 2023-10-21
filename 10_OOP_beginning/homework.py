bb367889c91384ce3f0dc0b3a8a63dee932eec99
Tatiana <tatiana-bondareva@inbox.ru>
2023-10-21 22:17:05 +0300

    Change homework 10

--- 10_OOP_beginning/homework.py
+++ 10_OOP_beginning/homework.py
@@ -20,70 +20,70 @@
 # Метод create_animal должен создавать и возвращать экземпляр животного заданного типа с переданными параметрами.
 
 
-# class Animal:
-#     def __init__(self, name):
-#         self.name = name
-#
-#
-# class Bird(Animal):
-#     def __init__(self, name, wingspan):
-#         super().__init__(name)
-#         self.wingspan = wingspan
-#
-#     def wing_length(self):
-#         return self.wingspan / 2
-#
-#
-# class Fish(Animal):
-#     def __init__(self, name, max_depth):
-#         super().__init__(name)
-#         self.max_depth = max_depth
-#
-#     def depth(self):
-#         if self.max_depth < 10:
-#             return 'Мелководная рыба'
-#         elif self.max_depth > 100:
-#             return 'Глубоководная рыба'
-#         else:
-#             return 'Средневодная рыба'
-#
-#
-# class Mammal(Animal):
-#     def __init__(self, name, weight):
-#         super().__init__(name)
-#         self.weight = weight
-#
-#     def category(self):
-#         if self.weight < 1:
-#             return 'Малявка'
-#         elif self.weight > 200:
-#             return 'Гигант'
-#         else:
-#             return 'Обычный'
-#
-#
-# class AnimalFactory:
-#     # @staticmethod
-#     # def create_animal(animal_type, *args):
-#     #     if animal_type == "Bird" and len(args) >= 2:
-#     #         return Bird(*args)
-#     #     elif animal_type == "Fish" and len(args) >= 2:
-#     #         return Fish(*args)
-#     #     elif animal_type == "Mammal" and len(args) >= 2:
-#     #         return Mammal(*args)
-#     #     else:
-#     #         raise ValueError("Недопустимый тип животного")
-#
-#
-# # Создание экземпляров животных
-# animal1 = AnimalFactory.create_animal('Bird', 'Орел', 200)
-# animal2 = AnimalFactory.create_animal('Fish', 'Лосось', 50)
-# animal3 = AnimalFactory.create_animal('Mammal', 'Слон', 5000)
-#
-# # Вывод результатов
-# print(animal1.wing_length())
-# print(animal2.depth())
-# print(animal3.category())
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+
+class Bird(Animal):
+    def __init__(self, name, wingspan):
+        super().__init__(name)
+        self.wingspan = wingspan
+
+    def wing_length(self):
+        return self.wingspan / 2
+
+
+class Fish(Animal):
+    def __init__(self, name, max_depth):
+        super().__init__(name)
+        self.max_depth = max_depth
+
+    def depth(self):
+        if self.max_depth < 10:
+            return 'Мелководная рыба'
+        elif self.max_depth > 100:
+            return 'Глубоководная рыба'
+        else:
+            return 'Средневодная рыба'
+
+
+class Mammal(Animal):
+    def __init__(self, name, weight):
+        super().__init__(name)
+        self.weight = weight
+
+    def category(self):
+        if self.weight < 1:
+            return 'Малявка'
+        elif self.weight > 200:
+            return 'Гигант'
+        else:
+            return 'Обычный'
+
+
+class AnimalFactory:
+    @staticmethod
+    def create_animal(animal_type, *args):
+        if animal_type == "Bird" and len(args) >= 2:
+            return Bird(*args)
+        elif animal_type == "Fish" and len(args) >= 2:
+            return Fish(*args)
+        elif animal_type == "Mammal" and len(args) >= 2:
+            return Mammal(*args)
+        else:
+            raise ValueError("Недопустимый тип животного")
+
+
+# Создание экземпляров животных
+animal1 = AnimalFactory.create_animal('Bird', 'Орел', 200)
+animal2 = AnimalFactory.create_animal('Fish', 'Лосось', 50)
+animal3 = AnimalFactory.create_animal('Mammal', 'Слон', 5000)
+
+# Вывод результатов
+print(animal1.wing_length())
+print(animal2.depth())
+print(animal3.category())
 
 
 # На вход программе подаются два списка, каждый из которых содержит 10 различных целых чисел.
